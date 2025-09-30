5f31a756fd93ba7f70897ce1a9beeb4388604f14
Lhoredel <lhoredeldollentas@gmail.com>
2025-09-30 18:46:32 +0800

    Mini-Max Sum.cs

---
+++ Mini-Max Sum.cs
@@ -0,0 +1,53 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<int> arr)
+    {
+        long total = 0;
+        long minVal = long.MaxValue;
+        long maxVal = long.MinValue;
+        
+        foreach (int num in arr)
+        {
+            total += num;
+            if (num < minVal) minVal = num;
+            if (num > maxVal) maxVal = num;
+        }
+        
+        Console.WriteLine((total - maxVal) + " " + (total - minVal));
+        
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.miniMaxSum(arr);
+    }
+}
